5abd330edf12c3d4dea4e3c18d1d8cc5eb27beca
Manuel Pichler <github@manuel-pichler.de>
2016-03-07 10:51:31 +0100

    Added: Rule for the different access restrictions.
    To be as fast as possible for the IDE usage, we have implemented all checks
    in a single PHPMD rule class, so that the source code get's traversed only
    one time.

---
+++ src/AccessRule.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace ArchitectureSniffer;
+
+use PHPMD\AbstractNode;
+use PHPMD\AbstractRule;
+use PHPMD\Node\ClassNode;
+use PHPMD\Rule\ClassAware;
+
+class AccessRule extends AbstractRule implements ClassAware
+{
+    /**
+     * @var array
+     */
+    private $patterns = [
+        [
+            '(^Spryker.+)',
+            '(^Pyz.+)',
+            '{type} {source} accesses {target} which violates rule "No call from Core to Project"'
+        ],
+        [
+            '(Spryker.*\\\\Yves\\\\.+)',
+            '(Spryker.*\\\\Zed\\\\.+)',
+            '{type} {source} accesses {target} which violates rule "No call from Yves to Zed"'
+        ],
+        [
+            '(Spryker.*\\\\Zed\\\\.+)',
+            '(Spryker.*\\\\Yves\\\\.+)',
+            '{type} {source} accesses {target} which violates rule "No call from Zed to Yves"'
+        ],
+        [
+            '(Spryker.*\\\\Shared\\\\.+)',
+            '(Spryker.*\\\\Zed\\\\.+)',
+            '{type} {source} accesses {target} which violates rule "No call from Shared to Zed"'
+        ],
+        [
+            '(Spryker.*\\\\Shared\\\\.+)',
+            '(Spryker.*\\\\Client\\\\.+)',
+            '{type} {source} accesses {target} which violates rule "No call from Shared to Client"'
+        ],
+        [
+            '(Spryker.*\\\\Shared\\\\.+)',
+            '(Spryker.*\\\\Yves\\\\.+)',
+            '{type} {source} accesses {target} which violates rule "No call from Shared to Yves"'
+        ],
+        //[
+        //    '(Spryker.*\\\\(Shared|Yves|Zed)\\\\Library\\\\.+)',
+        //    '(Spryker.*\\\\Yves\\\\.+)',
+        //    '{type} {source} accesses {target} which violates rule "No call libary-bundle to any other bundle"'
+        //],
+        [
+            '(Spryker.*\\\\Client\\\\.+)',
+            '(Spryker.*\\\\Zed\\\\.+)',
+            '{type} {source} accesses {target} which violates rule "No call from Client to Zed"'
+        ],
+        [
+            '(Spryker.*\\\\Client\\\\.+)',
+            '(Spryker.*\\\\Yves\\\\.+)',
+            '{type} {source} accesses {target} which violates rule "No call from Client to Yves"'
+        ],
+    ];
+
+    /**
+     * @param \PHPMD\AbstractNode $node
+     */
+    public function apply(AbstractNode $node)
+    {
+        $patterns = $this->collectPatterns($node);
+
+        $srcQName = sprintf('%s\\%s', $node->getNamespaceName(), $node->getName());
+
+        $this->applyPatterns($node, $srcQName, $patterns);
+
+        foreach ($node->getMethods() as $method) {
+            $this->applyPatterns(
+                $method,
+                sprintf('%s::%s()', $srcQName, $method->getName()),
+                $patterns
+            );
+        }
+    }
+
+    private function applyPatterns(AbstractNode $node, $srcQName, array $patterns)
+    {
+        foreach ($node->getDependencies() as $dependency) {
+            $targetQName = sprintf('%s\\%s', $dependency->getNamespaceName(), $dependency->getName());
+
+            foreach ($patterns as list($srcPattern, $targetPattern, $message)) {
+                if (0 === preg_match($srcPattern, $srcQName)) {
+                    continue;
+                }
+                if (0 === preg_match($targetPattern, $targetQName, $match)) {
+                    continue;
+                }
+
+                $this->addViolation(
+                    $node,
+                    [
+                        str_replace(
+                            ['{type}', '{source}', '{target}'],
+                            [ucfirst($node->getType()), $srcQName, $targetQName],
+                            $message
+                        )
+                    ]
+                );
+            }
+        }
+    }
+
+    private function collectPatterns(ClassNode $class)
+    {
+        $patterns = [];
+        foreach ($this->patterns as list($srcPattern, $targetPattern, $message)) {
+            if (preg_match($srcPattern, $class->getNamespaceName())) {
+                $patterns[] = [$srcPattern, $targetPattern, $message];
+            }
+        }
+        return $patterns;
+    }
+
+    private function applyNoCallFromLibraryBundleToOtherBundle(ClassNode $class)
+    {
+    }
+}

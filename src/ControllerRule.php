5e8f82509fd7c6d0b061922d59ed6f75b37dc1ac
Manuel Pichler <github@manuel-pichler.de>
2016-03-09 12:52:14 +0100

    Implemented: Controller action sniff

--- src/ControllerRule.php
+++ src/ControllerRule.php
@@ -3,11 +3,39 @@ namespace ArchitectureSniffer;
 
 use PHPMD\AbstractNode;
 use PHPMD\AbstractRule;
+use PHPMD\Node\MethodNode;
 use PHPMD\Rule\ClassAware;
 
 class ControllerRule extends AbstractRule implements ClassAware
 {
     public function apply(AbstractNode $node)
     {
+        if (0 === preg_match('(\\\\[^\\\\]+Controller$)', $node->getFullQualifiedName())) {
+            return;
+        }
+
+        foreach ($node->getMethods() as $method) {
+            $this->applyPublicMethodsHaveActionSuffix($method);
+        }
+    }
+
+    private function applyPublicMethodsHaveActionSuffix(MethodNode $method)
+    {
+        if ('Action' === substr($method->getName(), -6, 6)) {
+            return;
+        }
+        if ($method->isProtected() || $method->isPrivate()) {
+            return;
+        }
+
+        $this->addViolation(
+            $method,
+            [
+                sprintf(
+                    'The controller method %s is not suffixed with "Action" which violates rule "All public methods have the suffix *Action"',
+                    $method->getFullQualifiedName()
+                )
+            ]
+        );
     }
 }

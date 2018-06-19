1369d1021155f8fd422a6641ac662830ff226ddf
Karoly Gerner <karoly.gerner@spryker.com>
2018-06-19 10:42:53 +0000

    Adding class deprecation to DeprecationTrait, skipping class checks for deprecated classes

--- src/Client/NoClientInFrontendModuleRule.php
+++ src/Client/NoClientInFrontendModuleRule.php
@@ -2,12 +2,15 @@
 
 namespace ArchitectureSniffer\Client;
 
+use ArchitectureSniffer\Common\DeprecationTrait;
 use ArchitectureSniffer\SprykerAbstractRule;
 use PHPMD\AbstractNode;
 use PHPMD\Rule\ClassAware;
 
 class NoClientInFrontendModuleRule extends SprykerAbstractRule implements ClassAware
 {
+    use DeprecationTrait;
+
     protected const RULE = 'There should be no Client layer in frontend modules.';
 
     /**
@@ -26,6 +29,10 @@ class NoClientInFrontendModuleRule extends SprykerAbstractRule implements ClassA
     public function apply(AbstractNode $node)
     {
         if (preg_match('#^SprykerShop\\\\Client\\\\#', $node->getFullQualifiedName()) === 0) {
+            return;
+        }
+
+        if ($this->isClassDeprecated($node)) {
             return;
         }
 

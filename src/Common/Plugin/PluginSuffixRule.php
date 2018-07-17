d5683d06042514b34c0979632fb0d0e0375297d4
René Klatt <rene.klatt@spryker.com>
2018-07-17 13:13:03 +0000

    core-4299 update PluginRule to check also test files properly

--- src/Common/Plugin/PluginSuffixRule.php
+++ src/Common/Plugin/PluginSuffixRule.php
@@ -8,6 +8,7 @@ use PHPMD\Rule\ClassAware;
 class PluginSuffixRule extends AbstractPluginRule implements ClassAware
 {
     protected const EXPECTED_CLASS_NAME_SUFFIX = 'Plugin';
+    protected const EXPECTED_TEST_CLASS_NAME_SUFFIX = 'PluginTest';
 
     /**
      * @param \PHPMD\AbstractNode $node
@@ -21,9 +22,51 @@ class PluginSuffixRule extends AbstractPluginRule implements ClassAware
         }
 
         $className = $node->getName();
-        $suffix = substr($className, -strlen(static::EXPECTED_CLASS_NAME_SUFFIX));
-        if ($suffix !== static::EXPECTED_CLASS_NAME_SUFFIX) {
+
+        if (!$this->isSuffixValid($className)) {
             $this->addViolation($node, [$node->getName()]);
         }
+    }
+
+    /**
+     * @param string $className
+     *
+     * @return bool
+     */
+    protected function isSuffixValid(string $className): bool
+    {
+        if ($this->isSourcePluginSuffixValid($className)) {
+            return true;
+        }
+
+        if ($this->isTestPluginSuffixValid($className)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param string $className
+     *
+     * @return bool
+     */
+    protected function isSourcePluginSuffixValid(string $className): bool
+    {
+        $suffix = substr($className, -strlen(static::EXPECTED_CLASS_NAME_SUFFIX));
+
+        return ($suffix === static::EXPECTED_CLASS_NAME_SUFFIX);
+    }
+
+    /**
+     * @param string $className
+     *
+     * @return bool
+     */
+    protected function isTestPluginSuffixValid(string $className): bool
+    {
+        $suffix = substr($className, -strlen(static::EXPECTED_TEST_CLASS_NAME_SUFFIX));
+
+        return ($suffix === static::EXPECTED_TEST_CLASS_NAME_SUFFIX);
     }
 }

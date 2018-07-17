5a027ce901f095266676dcd4b158460c781dce50
René Klatt <rene.klatt@spryker.com>
2018-07-17 10:39:14 +0000

    added phpstan check, added .license file, updated travis

--- src/Zed/DependencyProvider/DependencyProviderMethodNameRule.php
+++ src/Zed/DependencyProvider/DependencyProviderMethodNameRule.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * MIT License
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace ArchitectureSniffer\Zed\DependencyProvider;
 
 use ArchitectureSniffer\Common\DependencyProvider\AbstractDependencyProviderRule;

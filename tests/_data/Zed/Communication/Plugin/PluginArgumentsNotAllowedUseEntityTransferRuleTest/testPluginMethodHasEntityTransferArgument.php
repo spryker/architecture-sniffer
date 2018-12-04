ec12b497eb1793909c7debe845f27fe3c95b29fd
Filip Sushko <filip.sushko@spryker.com>
2018-12-04 19:40:07 +0200

    DT-199: Added tests

---
+++ tests/_data/Zed/Communication/Plugin/PluginArgumentsNotAllowedUseEntityTransferRuleTest/testPluginMethodHasEntityTransferArgument.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Spyryker\Zed\Module\Communication\Plugin;
+
+use Generated\Shared\Transfer\TestModuleEntityTransfer;
+
+class TestPlugin
+{
+    /**
+     * @param \Generated\Shared\Transfer\TestModuleEntityTransfer $testModuleEntityTransfer
+     *
+     * @return void
+     */
+    protected function someMethod(TestModuleEntityTransfer $testModuleEntityTransfer): void
+    {
+    }
+}

c82d3796002633ea50d8dbdb373d598028dba5fc
Filip Sushko <filip.sushko@spryker.com>
2019-01-28 16:05:59 +0200

    Updated .license

--- tests/_data/Common/Plugin/NewPluginExtensionModuleTest/testRuleDoesNotApplyWhenPluginImplementsExtensionModuleInterface.php
+++ tests/_data/Common/Plugin/NewPluginExtensionModuleTest/testRuleDoesNotApplyWhenPluginImplementsExtensionModuleInterface.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
 namespace Spryker\Zed\Module\Communication\Plugin\NewPluginName;
 
 use Spryker\Zed\OtherModuleExtension\Dependency\Plugin\ExpanderPluginInterface;
